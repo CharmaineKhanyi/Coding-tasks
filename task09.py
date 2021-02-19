eb99699650ef9bf74f7e0b0ccec2a4e5336b3005
root <root@localhost.localdomain>
2021-02-19 05:57:52 +0200

    removing print statement for counting the vowels

--- task09.py
+++ task09.py
@@ -1,7 +1,7 @@
 def vowel(text):
 
     vowels = "aeiuoAEIOU"
-    print(len([letter for letter in text if letter in vowels]))
+    
     print([letter for letter in text if letter in vowels])
     
     
