735d94e4242d9aca3d6afbea9f70df175f06a753
root <root@localhost.localdomain>
2021-02-19 04:48:57 +0200

    spacing correction

--- task07.py
+++ task07.py
@@ -4,20 +4,14 @@ def convertCelsius(celsius):
     f = (celsius * 1.8)+32
     return f
 
-
-
 celsius = input("Enter a temperature in celsius ")
 celsius = float( celsius )
-
 # Pass our temperature variable to the function
 print(convertCelsius(celsius))
 f = convertCelsius( celsius )
 print('The temperature in Fahrenheit is: ', f)
 
-
-
 #convert temperature from Fahrenheit to celsius
-
 def convertFahrenheit(Fahrenheit):
     c = (Fahrenheit - 32)/1.8 
     return c
