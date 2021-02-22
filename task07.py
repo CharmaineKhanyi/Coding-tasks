cc6060a26ab00e77187ba8cf069643c0f658fdb9
CharmaineKhanyi <77413396+CharmaineKhanyi@users.noreply.github.com>
2021-02-22 22:29:44 +0200

    Update task07.py

--- task07.py
+++ task07.py
@@ -3,30 +3,10 @@
 def convertCelsius(celsius):
     f = (celsius * 1.8)+32
     return f
-
-
-
-celsius = input("Enter a temperature in celsius ")
-celsius = float( celsius )
-
-# Pass our temperature variable to the function
-print(convertCelsius(celsius))
-f = convertCelsius( celsius )
-print('The temperature in Fahrenheit is: ', f)
-
-
-
+convertCelsius(25)
 #convert temperature from Fahrenheit to celsius
 
 def convertFahrenheit(Fahrenheit):
     c = (Fahrenheit - 32)/1.8 
     return c
-
-Fahrenheit = input("Enter a temperature in Fahrenheit: ")
-Fahrenheit = float( Fahrenheit )
-
-print(convertFahrenheit(Fahrenheit))
-c = convertFahrenheit( Fahrenheit )
-print('The temperature in celsius is: ', c)
-
-
+convertFahrenheit(77)
