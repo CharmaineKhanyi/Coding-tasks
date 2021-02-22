6d901676c04df5a5ba9bab980420bc71a329bf49
CharmaineKhanyi <77413396+CharmaineKhanyi@users.noreply.github.com>
2021-02-22 22:58:15 +0200

    Update task09.py

--- task09.py
+++ task09.py
@@ -1,8 +1,6 @@
-def vowel(text):
-
-    vowels = "aeiuoAEIOU"
-    print(len([letter for letter in text if letter in vowels]))
-    print([letter for letter in text if letter in vowels])
-    
-    
-vowel('MyVowelString')
+def vowelsInString(string):
+    for letter in string:
+        if letter in "aAeEiIoOuU":
+            print(letter)
+            
+vowelsInString("Good day Khanyi")
