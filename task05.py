b02d6c146b2379b68b0355b0e0779a87ea222d7f
CharmaineKhanyi <77413396+CharmaineKhanyi@users.noreply.github.com>
2021-02-22 22:21:19 +0200

    Update task05.py

--- task05.py
+++ task05.py
@@ -1,22 +1,7 @@
-def areaOfTriangle( base, height ):
-    area = 0.5 * base * height
+def areaOfTriangle( side1, side2, side3 ):
+    area = 0.5 * (side1 + side2 + side3 )
     return area
 
-
-base = input("Enter triangle base length: ")
-height = input("Enter triangle height: ")
-
-
-
-
-base = float( base )
-height = float( height )
-
-
-# Pass our base and height variables to the function
-
-print('Area of triangle is', areaOfTriangle( base, height ))
-area = areaOfTriangle( base, height )
-print('The area of the triangle is', area)
+areaOfTriangle( 5, 4, 3 ))
 
 
