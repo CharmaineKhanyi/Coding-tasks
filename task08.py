f495034afb8b59ad7d75d9fa2e47582fa7a7b93f
CharmaineKhanyi <77413396+CharmaineKhanyi@users.noreply.github.com>
2021-02-22 22:50:05 +0200

    Update task08.py

--- task08.py
+++ task08.py
@@ -1,11 +1,15 @@
-def convert(number): 
+def convertMinutes(minutes):
+   h = minutes // 60
+   m = minutes % 60
+   if m > 1 and h > 1 :
+      time_string = "{} hours,{} minutes".format(h,m)
+   if m > 1 and h <= 1 :
+      time_string = "{} hour,{} minutes".format(h,m)
+   if m <= 1 and h > 1 :
+      time_string = "{} hours,{} minute".format(h,m)
+   if m <= 1 and h <= 1 :
+      time_string = "{} hour,{} minute".format(h,m)
+      
+   return time_string
    
-    hour = number/60
-    minutes = number % 60
-  
-      
-    return "%d:%02d" % (hour, minutes) 
-      
-
-n = 133
-print(convert(n)) 
+convertMinutes(123)
